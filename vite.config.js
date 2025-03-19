35ffd000dcb91a108874f947cbd9c85a5357b683
unknown <swingvotter@gmail.com>
2025-03-19 05:32:32 -1200

    updated the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // This allows access to the server from any network interface
-    port: 5173, // Specifies the port the server will run on
+    allowedHosts: ["advance-k-filtering.onrender.com"], // Add your Render URL here
   },
 });
